e6185ef199de9a87b968e0e5f73d60f9c530e842
jkkiveko <jameskk2004@gmail.com>
2024-08-17 11:46:48 -0400

    Fix floating point rounding errors with arm positioning mostly ;)

--- Assets/Scripts/InverseKinArmAtics.cs
+++ Assets/Scripts/InverseKinArmAtics.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class InverseKinArmAtics : MonoBehaviour
 {
-    public float smoothing = 0.5f;
+    public float smoothing = 0.0f;
     public Vector3 targetPoint;
     public Transform shoulder;
     public Transform upperArm;
@@ -13,9 +13,7 @@ public class InverseKinArmAtics : MonoBehaviour
     public Transform debugSphere;
     public Transform debugSphere2;
     private Vector3 oldElbowPosition;
-    private Vector3 newElbowPosition;
     private Vector3 oldHandPosition;
-    private Vector3 newHandPosition;
     private Transform playerControllerTransform;
 
     void Start() {
@@ -29,34 +27,42 @@ public class InverseKinArmAtics : MonoBehaviour
         float lowerArmLength = lowerArm.GetChild(0).localScale.y * 2;
         float armRadius = upperArmLength + lowerArmLength;
         // Gets the distance to the target point (side c in the cosine law)
-        newHandPosition = targetPoint;
-        if (Vector3.Distance(shoulder.position, newHandPosition) > armRadius){
-            newHandPosition = shoulder.position + (targetPoint - shoulder.position).normalized * armRadius;
+        Vector3 newHandPosition = targetPoint - shoulder.position; // ROUNDING ERROR!!!!!!!!!!!!!!
+        if (newHandPosition.magnitude > armRadius){
+            newHandPosition = newHandPosition.normalized * armRadius;
         }
-        float targetDistance = (newHandPosition - shoulder.position).magnitude;
+        float targetDistance = newHandPosition.magnitude;
         // Calculates the shoulder angle in radians (gamma in the cosine law)
         float shoulderAngle = Mathf.Acos((Mathf.Pow(upperArmLength, 2) + Mathf.Pow(targetDistance, 2) - Mathf.Pow(lowerArmLength, 2)) / (2 * upperArmLength * targetDistance));
-        shoulderAngle += Vector3.Angle(playerControllerTransform.forward, newHandPosition - shoulder.position) * Mathf.Deg2Rad;
+        shoulderAngle += Vector3.Angle(playerControllerTransform.forward, newHandPosition) * Mathf.Deg2Rad; // TRY CHANING TO VECTOR3.FORWARD!!!
         // The angle is NaN when it should be exactly 180 degrees
         if (float.IsNaN(shoulderAngle)){
             shoulderAngle = Mathf.PI / 2.0f;
         }
         // Uses basic trig to calculate the elbow's position
-        newElbowPosition = shoulder.position + -playerControllerTransform.right * upperArmLength * Mathf.Sin(shoulderAngle) + playerControllerTransform.forward * upperArmLength * Mathf.Cos(shoulderAngle);
-        debugSphere.position = newHandPosition;
+        Vector3 newElbowPosition = -playerControllerTransform.right * upperArmLength * Mathf.Sin(shoulderAngle) + playerControllerTransform.forward * upperArmLength * Mathf.Cos(shoulderAngle);
+        debugSphere.position = shoulder.position + newHandPosition;
         debugSphere2.position = targetPoint;
 
         // Updates the arms to match the math
         // Also smooths the movement of the visual arms
-        Vector3 elbowPosition = Vector3.Lerp(oldElbowPosition, newElbowPosition, smoothing);
-        Vector3 handPosition = Vector3.Lerp(oldHandPosition, newHandPosition, smoothing);
+        Vector3 elbowPosition = Vector3.Lerp(shoulder.position + newElbowPosition, oldElbowPosition, smoothing);
+        Vector3 handPosition = Vector3.Lerp(shoulder.position + newHandPosition, oldHandPosition, smoothing);
 
-        oldElbowPosition = elbowPosition;
-        oldHandPosition = handPosition;
-
-        upperArm.position = shoulder.position;
         upperArm.LookAt(elbowPosition);
         lowerArm.position = elbowPosition;
         lowerArm.LookAt(handPosition);
+
+        oldElbowPosition = elbowPosition;
+        oldHandPosition = handPosition;
+    }
+
+    //deprecated
+    Vector3 RoundVector3 (Vector3 toRound) {
+        Vector3 toReturn = new Vector3();
+        toReturn.x = Mathf.Round(toRound.x * 100) / 100;
+        toReturn.y = Mathf.Round(toRound.y * 100) / 100;
+        toReturn.z = Mathf.Round(toRound.z * 100) / 100;
+        return toReturn;
     }
 }
