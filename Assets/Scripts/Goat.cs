d2e22d7a1fb2d16deb1bcded9f10d67793df319a
gw-n <gwendolensmh@gmail.com>
2024-08-19 14:08:04 -0400

    title screen goat

---
+++ Assets/Scripts/Goat.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goat : MonoBehaviour
+{
+
+    private float maxHeight = 0.025f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3 (transform.position.x, (float) 0.21 +  maxHeight * Mathf.Sin(Time.time));
+    }
+}
