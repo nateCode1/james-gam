b2c2e9bb33260495f71e44ba34ef528bda90ffff
gw-n <gwendolensmh@gmail.com>
2024-08-19 19:27:44 -0400

    arm fly off good

--- Assets/Scripts/Fishing/Arm.cs
+++ Assets/Scripts/Fishing/Arm.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Arm : MonoBehaviour
 {
     public float rotational_accel = 0.1f;
     public float rotational_speed = 0.0f;
     private Rigidbody2D _rigidbody;
+    private float timeElapsed = 0.0f;
     public bool isCasted = false;
     public bool didReset = true;
+    public Fish fish;
 
     public Bobber theBobber;
 
@@ -31,6 +34,11 @@ public class Arm : MonoBehaviour
             if(Input.GetMouseButtonUp(0)) {
                 _rigidbody.constraints &= (~RigidbodyConstraints2D.FreezePositionX & ~RigidbodyConstraints2D.FreezePositionY);
                 _rigidbody.velocity = new Vector3(10,10,0);
+                fish.fishAscent();
+                timeElapsed += Time.deltaTime;
+                if(timeElapsed > 2f) {
+                    SceneManager.UnloadSceneAsync("FishingMinigame");
+                }
             }
         }
         if(rotational_speed > 0){
