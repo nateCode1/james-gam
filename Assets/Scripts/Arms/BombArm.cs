3cec377fbe2bfaf8a8a9acf3068c698f98e3c215
jkkiveko <jameskk2004@gmail.com>
2024-08-19 18:52:24 -0400

    Add reload time to bomb

--- Assets/Scripts/Arms/BombArm.cs
+++ Assets/Scripts/Arms/BombArm.cs
@@ -7,14 +7,26 @@ public class BombArm : InverseKinArmAtics
 {
     public GameObject bombTemplate;
     public GameObject handBomb;
+    public float reloadTime = 3;
+    private float timeSinceLastShot = 0;
     private GameObject bomb; // could be a list of bombs if we want more than one to be alive at a time
+
+    public override void VisualUpdate(Vector3 elbowPosition, Vector3 handPosition, float lowerArmLength){
+        // this isn't totally visual... sorry!
+        timeSinceLastShot += Time.deltaTime;
+        if (timeSinceLastShot > reloadTime && !handBomb.activeSelf && !bomb) {
+            handBomb.SetActive(true);
+        }
+        base.VisualUpdate(elbowPosition, handPosition, lowerArmLength);
+    }
+
     public override void Pressed() {
         bomb = Instantiate(bombTemplate, actualHandPos, Quaternion.identity);
         handBomb.SetActive(false);
     }
     public override void LetGo() {
         if (bomb) {
-            handBomb.SetActive(true);
+            timeSinceLastShot = 0;
             bomb.GetComponent<Explode>().ExplodeBomb();
             Destroy(bomb);
         }
